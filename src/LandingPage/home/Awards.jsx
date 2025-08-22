300dbcac26972a7e22d7c542f38b27c9ec716955
Pankajdey018 <pankajdey.olduser@gmail.com>
2025-08-22 13:25:59 +0530

    added hero and award components

--- src/LandingPage/home/Awards.jsx
+++ src/LandingPage/home/Awards.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React from "react";
 
 function Awards() {
-    return ( <h1>Award</h1> );
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-6 p-5">
+          <img src="\media\largestBroker.svg" />
+        </div>
+        <div className="col-6 p-5 mt-3">
+          <h1>Largest stock broker in India</h1>
+          <p className="mb-5">
+            2+ million Zerodha clients contribute to over 15% of all retail
+            order volumes in india daily by trading and investing in:{" "}
+          </p>
+          <div className="row">
+            <div className="col-6 ">
+              <ul>
+                <li><p>Future and Options</p></li>
+                <li><p>Commodity and derivatives</p></li>
+                <li><p>Currency derivatives</p></li>
+              </ul>
+            </div>
+            <div className="col-6 ">
+              <ul>
+                <li><p>Stocks and IPOs</p></li>
+                <li><p>Direct mutual funds</p></li>
+                <li><p>Bonds and Goverment. Securities</p></li>
+              </ul>
+            </div>
+            <img src="\media\pressLogos.png" style={{width: "90%"}} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Awards;
--- src/LandingPage/home/Hero.jsx
+++ src/LandingPage/home/Hero.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from "react";
 
 function Hero() {
-    return ( <h1>hero product</h1> );
+  return( 
+    <div className="container p-5 mb-5">
+        <div className="row text-center">
+            <img src="\media\homeHero.png" alt="Hero image" className="mb-5"/>
+            <h1 className="mt-5">Invest in everything</h1>
+            <p>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
+            <button style={{width: "20%", margin: "0 auto"}} className="fs-5 p-3 btn btn-primary">Signup now</button>
+        </div>
+    </div>
+  )
 }
 
 export default Hero;
