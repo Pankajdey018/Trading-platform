fa10afe3345bdce28390b6af13930ea56adee830
Pankajdey018 <pankajdey.olduser@gmail.com>
2025-08-24 02:36:28 +0530

    completed home section code

--- src/LandingPage/home/Education.jsx
+++ src/LandingPage/home/Education.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React from "react";
 
 function Education() {
-    return ( <h1>Education</h1> );
+  return (
+    <div className="container m-5">
+      <div className="row">
+        <div className="col">
+          <img src="\media\education.svg" style={{width: "75%"}} />
+        </div>
+        <div className="col">
+          <h1 className="mb-3 fs-2">Free and open market education</h1>
+
+          <p>
+            Varsity, the largest online stock market education book in the world
+            covering everything from the vasics to advanced trading
+          </p>
+
+          <a href="" style={{ textDecoration: "none" }}>
+            Versity &rarr;
+          </a>
+
+          <p className="mt-5">
+            TradingQ&A, the most active trading and investment community in
+            India for all you market related queries.
+          </p>
+
+          <a href="" style={{ textDecoration: "none" }}>
+            Trading Q&A&rarr;
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Education;
--- src/LandingPage/home/Pricing.jsx
+++ src/LandingPage/home/Pricing.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React from "react";
 
 function Pricing() {
-    return ( <h1>Pricing</h1> );
+  return (
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-5">
+          <h1 className="mb-5 fs-2">Unbeatable pricing</h1>
+
+          <p className="mb-2 text-muted">
+            We pioneered the concept of discount broking and price transparency
+            in India. Flat fees and no hidden charges.
+          </p>
+
+          <a href="" style={{ textDecoration: "none" }}>
+            See Pricing &rarr;
+          </a>
+        </div>
+        <div className="col-2"></div>
+        <div className="col-5 ">
+          <div className="row text-center">
+            <div className="col border p-4">
+              <h1>&#8377;0</h1>
+              <p>Free equity delivery and direct mutual funds</p>
+            </div>
+            <div className="col border p-4">
+              <h1>&#8377;20</h1>
+              <p>Intraday and F&O</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Pricing;
